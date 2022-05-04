9ef77598193079c40c5fa3d416dfa04f197d9369
Jonathas <jonathasx57@gmail.com>
2022-05-04 08:43:57 -0300

    Adicionando arquivos

---
+++ nomeDiferente.java
@@ -0,0 +1,9 @@
+public class qualquerNome{
+
+    public static void main(String[] args){
+  
+      System.out.print("Oioioi");
+      
+    }
+  
+  }
---
+++ nomeTimeMin.java
@@ -0,0 +1,8 @@
+public class nomeTimeMin {
+  
+    public static void main(String[] args) {
+      system.out.println("Jonathas e Samira");
+      system.out.println("Brasil");
+  }
+  
+}
